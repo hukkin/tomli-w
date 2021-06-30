caa5cc2be87ba50a3396619b0eb4d555e4861c2f
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-06-30 18:28:34 +0300

    Bump version: 0.0.1 → 0.1.0

--- tomli_w/__init__.py
+++ tomli_w/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ("dumps",)
-__version__ = "0.0.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from tomli_w._writer import dumps
