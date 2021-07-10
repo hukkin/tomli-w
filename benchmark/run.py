680bb922bc48867479ccd8045412e0eea2377aa9
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-07-10 13:06:32 +0300

    Improve benchmark and add tox env for profiling

--- benchmark/run.py
+++ benchmark/run.py
@@ -1,46 +1,21 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from datetime import datetime, timedelta, timezone
+from pathlib import Path
 import timeit
 
 import pytomlpp
 import qtoml
-import rtoml
+
+# import rtoml
 import toml
+import tomli
 import tomlkit
 
 import tomli_w
 
-test_data = {
-    "string": "blaa blaa",
-    "string with newline and tab": "blaa\nblaa\tbluuu",
-    "string with apostrophe": "dii daa 'apostrophe' here",
-    "string with quote": 'dii daa "quote" here',
-    "string-with-weird-char": "weird char \u0fff",
-    "integer": 123,
-    "float": 123.456,
-    "boolean": True,
-    "array": [False, True, False],
-    "table": {
-        "offset-datetime-utc": datetime(1999, 12, 24, 23, 59, 59, tzinfo=timezone.utc),
-        "offset-datetime-non-utc": datetime(
-            1999, 12, 24, 23, 59, 59, tzinfo=timezone(timedelta(hours=2, minutes=30))
-        ),
-        "local-datetime": datetime(1999, 12, 24, 23, 59, 59),
-        # TODO: uncomment when rtoml supports local times and dates
-        # "local date": date(1999, 12, 24),
-        # "local time": time(23, 59, 59),
-        "inner-table": {"empty array": []},
-    },
-    "table2": {
-        "big int": 52443235623986,
-        "inf float": float("inf"),
-        "nan float": float("nan"),
-        "negative inf": float("-inf"),
-    },
-    "array-of-tables": [{"number": 31}, {"number": 5}, {"number": 7}, {"number": 99}],
-}
+test_data_file = Path(__file__).parent / "data.toml"
+test_data = tomli.loads(test_data_file.read_text(encoding="utf-8"))
 # Make the game fair for tomlkit by converting `test_data` to a
 # tomlkit native type before measurements.
 tomlkit_test_data = tomlkit.item(test_data)
@@ -79,7 +54,7 @@ def run(run_count: int) -> None:
     print("-" * col_width[0] + "-+-" + "-" * col_width[1] + "-+-" + col_width[2] * "-")
     # fmt: off
     baseline = benchmark("pytomlpp", run_count, lambda: pytomlpp.dumps(test_data), col_width)  # noqa: E501
-    benchmark("rtoml", run_count, lambda: rtoml.dumps(test_data), col_width, compare_to=baseline)  # noqa: E501
+    # benchmark("rtoml", run_count, lambda: rtoml.dumps(test_data), col_width, compare_to=baseline)  # noqa: E501
     benchmark("tomli", run_count, lambda: tomli_w.dumps(test_data), col_width, compare_to=baseline)  # noqa: E501
     benchmark("toml", run_count, lambda: toml.dumps(test_data), col_width, compare_to=baseline)  # noqa: E501
     benchmark("tomlkit", run_count, lambda: tomlkit.dumps(tomlkit_test_data), col_width, compare_to=baseline)  # noqa: E501
---
+++ tests/test_for_profiler.py
@@ -0,0 +1,28 @@
+"""Test for profiling.
+
+This test can be useful for profiling, as most of the execution time
+will be spent writing TOML instead of managing pytest execution
+environment. To get and read profiler results:
+  - `tox -e profile`
+  - `firefox .tox/prof/combined.svg`
+"""
+import os
+from pathlib import Path
+
+import tomli
+
+import tomli_w
+
+path = Path(__file__).parent.parent / "benchmark" / "data.toml"
+benchmark_toml = path.read_text("utf-8")
+data = tomli.loads(benchmark_toml)
+
+
+def test_for_profiler():
+    # increase the count here to reduce the impact of
+    # setting up pytest execution environment. Let's keep
+    # the count low by default because this is part of the
+    # standard test suite.
+    iterations = int(os.environ.get("PROFILER_ITERATIONS", 1))
+    for _ in range(iterations):
+        tomli_w.dumps(data)
